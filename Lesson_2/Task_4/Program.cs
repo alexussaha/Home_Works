cb10db24ef5878a93071bb4856938587a42f54c0
Александр <ussov39@gmail.com>
2019-07-29 13:45:48 +0300

    Выполнено четвёртое задание.

---
+++ Lesson_2/Task_4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace Task_4
+{
+    class Program
+    {
+        static string login = "root";
+        static string password = "GeekBrains";
+        static bool CheckTheEntrence(string usrlogin, string usrpassword)
+        {
+            bool log, pas;
+            log = String.Equals(usrlogin, login);
+            if (log) return pas = String.Equals(usrpassword, password);
+            return log;
+        }
+        static void Main(string[] args)
+        {
+            
+            int i = 0;
+            bool right = false;
+            do
+            {
+                string userLogin;
+                string userPassword;
+                if (i != 0) Console.WriteLine($"У Вас осталось {3-i} попыток.");
+                Console.Write("Введите логин: ");
+                userLogin = Console.ReadLine();
+                Console.Write("Введите пароль: ");
+                userPassword = Console.ReadLine();
+                right = CheckTheEntrence(userLogin, userPassword);
+                i++;
+
+            } while (!(right || i==3));
+            if (right == false) Console.WriteLine("Ваши попытки закончились.");
+              else Console.WriteLine("Вход выполнен.");
+            Console.ReadKey();
+        }
+    }
+}
