cb4ea067a66c959078fd5a28809fa5d06fa2fd8c
Александр <ussov39@gmail.com>
2019-07-29 15:38:20 +0300

    Выполнил шестое задание.

---
+++ Lesson_2/Task_6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task_6
+{
+    class Program
+    {
+        static bool IsTheNumberGood(int number)
+        {
+            int checkNumber = number;
+            int sum = 0;
+            while (checkNumber != 0)
+            {
+                sum += checkNumber % 10;
+                checkNumber = checkNumber / 10;
+            }
+            if (number % sum == 0)    return true;
+            
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            DateTime start = DateTime.Now;
+            int count = 0;
+            for(int i = 1; i<= 1000000000; i++)
+            {
+                if (IsTheNumberGood(i)) count++;
+            }
+
+            DateTime finish = DateTime.Now;
+            Console.WriteLine(count);
+            Console.WriteLine($"Время: {finish - start}");
+            Console.ReadKey();
+
+        }
+    }
+}
