6882bd265db57c600790a602e830c4fd482fd346
Александр <ussov39@gmail.com>
2019-07-29 12:16:05 +0300

    Добавлено решение второго задания.

--- Lesson_2/Task_2/Program.cs
+++ Lesson_2/Task_2/Program.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace Task_2
 {
+   
     class Program
     {
+        static int CountOfDigits(int a)
+        {
+            int count = 0;
+            while (a != 0)
+            {
+                a = a / 10;
+                count++;
+            }
+            return count;
+        }
         static void Main(string[] args)
         {
+            int a;
+            Console.WriteLine("Введите число");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Колличество цифр в чиле: {CountOfDigits(a)}");
+            Console.ReadKey();
         }
     }
 }
