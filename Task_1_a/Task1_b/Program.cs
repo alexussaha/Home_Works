c3acae694be87bd6fd77faf9647177507ffd7c62
Александр <ussov39@gmail.com>
2019-08-02 12:15:16 +0300

    Сделал первое и второе задание

---
+++ Task_1_a/Task1_b/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+
+namespace Task1_b
+{
+    class Complex
+    {
+        double im;
+        double re;
+        public Complex Plus(Complex x2)
+        {
+            Complex x3 = new Complex();
+            x3.im = x2.im + this.im;
+            x3.re = x2.re + this.re;
+            return x3;
+        }
+        public Complex Minus(Complex x2)
+        {
+            Complex x3 = new Complex();
+            x3.im = x2.im - this.im;
+            x3.re = x2.re - this.re;
+            return x3;
+        }
+        public Complex Multi(Complex x2)
+        {
+            Complex x3 = new Complex();
+            x3.im = re * x2.im + im * x2.re;
+            x3.re = re * x2.re - im * x2.im;
+            return x3;
+        }
+        public override string ToString()
+        {
+            return re + "+" + im + "i";
+        }
+        public double Im {
+            get { return im; }
+            set
+            {
+                if (value >= 0) im = value;
+            }
+        }
+        public double Re
+        {
+            get { return re; }
+            set
+            {
+                if (value >= 0) re = value;
+            }
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex complex1 = new Complex();
+            complex1.Re = 1;
+            complex1.Im = 1;
+
+            Complex complex2 = new Complex();
+            complex2.Re = 2;
+            complex2.Im = 2;
+
+            Complex result = complex1.Plus(complex2);
+            Console.WriteLine(result.ToString());
+            result = complex2.Minus(complex1);
+            Console.WriteLine(result.ToString());
+            result = complex1.Multi(complex2);
+            Console.WriteLine(result.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_1_a/Task_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void IfOdd(int n, ref int s)
+        {
+            if (n % 2 > 0)
+            {
+                s += n;
+            }
+        }
+      
+        static void Main(string[] args)
+        {
+            string number;
+            int n, s = 0;
+            Console.WriteLine("Вводите целые числа. Для завершения программы введите ноль.");
+            do
+            {
+                number = Console.ReadLine();
+                if (int.TryParse(number, out n))
+                {
+                   n = int.Parse(number);
+                   IfOdd(n, ref s);
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Неверный ввод (введите цедлое чило)");
+                    n = 1;
+                }
+            } while (n != 0);
+            Console.WriteLine($"Сумма нечётных чисел равна: {s}");
+            Console.ReadKey();
+        }
+    }
+}
